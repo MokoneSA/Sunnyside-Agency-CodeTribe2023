5a581e2ea6b770be3a1f58426484b0223104e629
MokoneSA <simonlephotojr@gmail.com>
2023-03-02 12:38:22 +0200

    first commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,50 +1,62 @@
-/*
-The code below will change the heading with id = "mainHeading"
-and insert a image with id = "imgArrow" in heading section:
-*/
 
 // Change heading for the header section
 const mainHeading = document.getElementById('mainHeading').innerHTML = "We are creatives";
+
 
 // Insert image under the heading
 const headingImage = document.getElementById("imgArrow");
 headingImage.src ="./images/icon-arrow-down.svg";
 
 
-
-/*
-The code below will change the section one headings with id's sectionOneHeadine
-and sectionTwoHeading, it will also insert images for each section with Id's
-
-*/
-
 // Change headings for section one and two 
 const sectionOneHeading = document.getElementById('sectionOneHeading').innerHTML = "Transform your brand";
+
 const sectionTwoHeading = document.getElementById('sectionTwoHeading').innerHTML = "Stand out to the right audience";
+
 
 // Change paragraphs for section one and two 
 const sectionOneParagraph = document.getElementById('sectionOnePara').innerHTML = "We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.";
+
 const sectionTwoParagraph = document.getElementById('sectionTwoPara').innerHTML = "Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we’ll build and extend your brand in digital places.";
+
 
 // Insert image for section one
 const sectionOneImage = document.getElementById("sectionOneImage");
 sectionOneImage.src ="./images/desktop/image-transform.jpg";
 sectionOneImage.setAttribute("width", "100%");
 
+
 // Insert image for section two
 const sectionTwoImage = document.getElementById("sectionTwoImage");
 sectionTwoImage.src ="./images/desktop/image-stand-out.jpg";
 sectionTwoImage.setAttribute("width", "100%");
 
+
 // Insert links for section one and two
 const sectionOneLink = document.getElementById('sectionOneLink').innerHTML = "Learn More";
+
 const sectionTwoLink = document.getElementById('sectionTwoLink').innerHTML = "Learn More";
 
 
 // change heading for section three
 const sectionThreeHeading1 = document.getElementById('sectionThreeHeading1').innerHTML = "Graphic design";
+
 const sectionThreeHeading2 = document.getElementById('sectionThreeHeading2').innerHTML = "Photography";
+
 
 // Insert text into section three paragraphs
 const sectionThreePara1 = document.getElementById('sectionThreePara1').innerHTML = "Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential clients’ attention.";
+
 const sectionThreePara2 = document.getElementById('sectionThreePara2').innerHTML = "Increase your credibility by getting the most stunning, high-quality photos that improve your business image.";
+
+
+// Client testimonials heading
+const client_header = document.getElementById('client_header').innerHTML = "Client testimonials";
+
+
+// Clients testimonials paragraphs
+const first_clientP = document.getElementById('first_clientP').innerHTML = "We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit.";
+
+const second_clientP = document.getElementById('second_clientP').innerHTML = "Sunnyside’s enthusiasm coupled with their keen interest in our brand’s success made it a satisfying and enjoyable experience.";
+
+const third_clientP = document.getElementById('third_clientP').innerHTML = "Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!";
